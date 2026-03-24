77440d8e0a84b01a8bc5c673859d9a7c3231be64
Ben Shipp <130992763+Fimy32@users.noreply.github.com>
2026-03-24 16:07:19 +0000

    Update MainActivity.kt

--- app/src/main/java/com/example/networkcommunication/MainActivity.kt
+++ app/src/main/java/com/example/networkcommunication/MainActivity.kt
@@ -1,7 +1,17 @@
-package com.example.networkcommunication
+package com.example.myapplication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
@@ -11,14 +21,18 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.networkcommunication.ui.theme.NetworkCommunicationTheme
+import com.github.kittinunf.fuel.core.Parameters
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.gson.responseObject // for GSON - uncomment when needed
+import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.result.Result
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +42,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             var artist by remember { mutableStateOf("") }
             var results by remember { mutableStateOf("") }
-            NetworkCommunicationTheme {
+            MyApplicationTheme {
                 Column {
                     Text("Search For Artist")
                     TextField(
@@ -40,6 +54,7 @@ class MainActivity : ComponentActivity() {
                     }
                     Text("Results: $results")
 
+                    NetworkComm(artist.toString())
                 }
             }
         }
@@ -47,18 +62,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun NetworkComm(artist: String) {
+    var responseText by remember { mutableStateOf("") }
+    Column {
+        Button( onClick = {
+            // URL which returns JSON describing points of interest
+            var url = "http://10.0.2.2:3000/artist/" + artist
+            url.httpGet().response { request, response, result ->
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NetworkCommunicationTheme {
-        Greeting("Android")
+                when(result) {
+                    is Result.Success -> {
+                        // result.get() gives ByteArray, decode to string
+                        responseText = result.get().decodeToString()
+                    }
+
+                    is Result.Failure -> {
+                        // is failure if HTTP error
+                        responseText = "ERROR ${result.error.message}"
+                    }
+                }
+            }
+        }) {
+            Text("Get data from Web!")
+        }
+        Text(responseText)
     }
 }
-//js
+
+
+
